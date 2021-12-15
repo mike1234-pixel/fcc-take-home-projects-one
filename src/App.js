c8df2377632c60b7e30254056a47ef038fed61f1
Michael Tandy <miketandy@protonmail.com>
2021-12-15 16:42:12 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { RiArrowRightSFill } from "react-icons/ri";
 
-function App() {
+
+
+const App = (props) => {
+  
+
+  // maps is type 
+  const [data, setData] = useState([props.forumPosts])
+  const [forumPosts, setForumPosts] = useState(data[0].topic_list.topics);
+
+  console.log(forumPosts)
+
+  function msToTime(duration) {
+      let minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  
+      if (hours > 0) {
+        return hours + "h " + minutes + "m";
+      } else {
+        return minutes + "m";
+      }
+  }
+
+  function fccCategory(categoryId) {
+
+    let category
+
+    switch(categoryId) {
+      case 299:
+        category = 'Career Advice'
+        break;
+      case 409:
+        category = 'Project Feedback'
+        break;
+      case 421:
+        category = 'JavaScript'
+        break;
+      case 423:
+        category = 'HTML-CSS'
+        break;
+      case 424:
+        category = 'Python'
+        break;
+      default:
+        category = 'General'
+    }
+
+    return category
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>freeCodeCamp Forum</h1>
+          <div className="posts">
+            {forumPosts.map((post) => (
+              <a key={post.id} href={`https://forum.freecodecamp.org/t/${post.slug}`} className="post">
+                <div className="post__content">
+                  <h3>{post.title}</h3>
+                  <img src={post.image_url}/>
+                  <p>Replies: {parseInt(post.posts_count) - 1}</p>
+                  <p>Last Active: {msToTime(Date.now() - Date.parse(post.last_posted_at))} ago</p>
+                  <span>
+                    <RiArrowRightSFill></RiArrowRightSFill>
+                  </span>
+                  <p className={`post__category ${fccCategory(post.category_id).toLowerCase().replace(' ', '-')}`}>
+                    {fccCategory(post.category_id)}
+                    
+
+                  </p>
+                </div>
+              </a>
+              
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
+
+
+
 }
 
 export default App;
+
+// need to return the fields individually, not the whole object
+
+
+// category_id
+// 299 === career advice
+// 423 === html-css
+// 424 === python
+// 421 === javascript
+// 1 or other === general
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const requestOne = axios.get('https://forum-proxy.freecodecamp.rocks/latest');
+
+axios
+  .all([requestOne])
+  .then(
+    axios.spread((...responses) => {
+      const forumPosts = responses[0];
+      ReactDOM.render(
+        <App forumPosts={forumPosts.data}/>
+      , document.getElementById('root'));
+    })
+  )
+  .catch(errors => {
+    console.error(errors);
+  });
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
